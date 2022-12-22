3fcbae1afb5d3768fa53e6eece449748f0eb682b
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2022-12-22 15:11:29 +0100

    moved create_cards to game.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
 __author__ = "7987847, Werner, 7347119, Fajst, 1234567, dalimeli"
 
 
-def create_cards():
-    ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack',
-                'Queen', 'King']
-    suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
-
-    card_deck = {}
-
-    for suit in suits:
-        for rank in ranks:
-            card = f'{rank} of {suit}'
-            card_deck[card] = None
-    return card_deck
-
-card_deck = create_cards()
-print(card_deck)
